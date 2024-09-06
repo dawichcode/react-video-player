0ef4aa4afee08151b92cbec640d9ca252efd8508
dawichcode <muvleltd@gmail.com>
2024-09-06 08:50:31 +0100

    bug fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.tsx'),
+      name: 'gc-video-player',
+      fileName: (format) => `gc-video-player.${format}.js`,
+    },
+    rollupOptions: {
+      // Externalize dependencies that shouldn’t be bundled into the library
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
